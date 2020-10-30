32b1866696b80c4875bb559555f05e95a849dbcf
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-30 13:35:48 -0700

    declare 'finalChain' array, restructure inputs for User use, rename 'inputAsNumber' to 'input', rename 'mixedArray' to 'finalChain', create callback function 'chainTheseFunctions' for the UI, write the UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
+// BUSINESS LOGIC
 let numberArray = [];
+let finalChain = [];
+
 function positiveRange(rangeInput) {
   while (rangeInput > numberArray.length - 1) {
     numberArray.push(numberArray.length);
   };
 };
+
 function negativeRange(rangeInput) {
   while ((rangeInput * -1) > (numberArray.length - 1)) {
     numberArray.push(numberArray.length * -1);
@@ -11,34 +15,53 @@ function negativeRange(rangeInput) {
 };
 
 function buildABear(input) {
-  const inputAsNumber = parseInt(input);
-  if (isNaN(inputAsNumber)) {
-    numberArray = ["is not a number"];
+  if (isNaN(input)) {
+    finalChain = ["is not a number"];
   }
   else { 
     numberArray = [0]
-    if (inputAsNumber > 0) {
-      positiveRange(inputAsNumber);
-    } else if (inputAsNumber < 0) {
-      negativeRange(inputAsNumber);
-    } else if (inputAsNumber === 0) {
-      return numberArray
+    if (input > 0) {
+      positiveRange(input);
+    } else if (input < 0) {
+      negativeRange(input);
+    } else if (input === 0) {
+      finalChain = [0]
     } else {
-      return "is otherwise broken"
+      finalChain = ["is otherwise broken"]
     }
   }
 };
-buildABear();
 
-let mixedArray = [];
-numberArray.forEach(function(input) {
-  if (/3/.test(input) === true) {
-    mixedArray.push("Contains 3");
-  } else if (/2/.test(input) === true) {
-    mixedArray.push("Contains 2");
-  } else if (/1/.test(input) === true) {
-    mixedArray.push("Contains 1");
-  } else {
-    mixedArray.push(input);
-  };
+
+function chainTheseFunctions(input) {
+  numberArray = [];
+  finalChain = [];
+  buildABear(input);
+    numberArray.forEach(function(input) {
+    if (/3/.test(input) === true) {
+      finalChain.push("Contains 3");
+    } else if (/2/.test(input) === true) {
+      finalChain.push("Contains 2");
+    } else if (/1/.test(input) === true) {
+      finalChain.push("Contains 1");
+    } else if (input !== 0) {
+      finalChain.push(input);
+    } else {
+      return "error"
+    };
+  });
+};
+
+
+// USER INTERFACE LOGIC
+$(document).ready(function() {
+  $("#box").submit(function(event) {
+    event.preventDefault();
+    const userNumber = parseInt($("input#number").val());
+    chainTheseFunctions(userNumber);    
+    console.log(numberArray);
+    console.log(finalChain);
+    $("#neighbor").text(finalChain);
+    $("#output").show();
+  });
 });
